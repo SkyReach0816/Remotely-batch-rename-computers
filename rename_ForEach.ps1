f81e4fa1dd6a24bb74c97bc2699e13622d15bdf7
SpectruM <92255068+SkyReach0816@users.noreply.github.com>
2024-05-15 22:47:22 +0800

    rename_ForEach.ps1

--- rename_ForEach.ps1
+++ rename_ForEach.ps1
@@ -1,7 +1,6 @@
-$user = "danny"
-$password = "d618" | ConvertTo-SecureString -AsPlainText -Force
+$user = "user_name"
+$password = "password" | ConvertTo-SecureString -AsPlainText -Force
 $Credential = New-object System.Management.Automation.PSCredential($user,$password)
-#New-PSSession -ComputerName 192.168.50.105 -Credential  $Credential
 $ipnewname = Import-Csv -Path c:\pshell2\ipnewname.csv
 $ipnewname|ForEach{
 Rename-Computer -ComputerName  $_.ip  -NewName  $_.newname  -LocalCredential  $Credential  -Restart
